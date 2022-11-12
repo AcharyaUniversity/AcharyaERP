1a33473713747009c755ff63f68d005581902841
VIVEK PANDEY <prathmeshherlekar@gmail.com>
2022-11-12 10:14:31 +0530

    designation,shift

--- src/App.jsx
+++ src/App.jsx
@@ -15,13 +15,10 @@ import FormExample from "./containers/examples/FormExample";
 // Master pages
 import InstituteMaster from "./pages/masters/InstituteMaster";
 import NavigationMaster from "./pages/masters/NavigationMaster";
+
 import DesignationMaster from "./pages/masters/DesignationMaster";
-import TranscriptMaster from "./pages/masters/TranscriptMaster";
-import InventoryMaster from "./pages/masters/InventoryMaster";
+
 import ShiftMaster from "./pages/masters/ShiftMaster";
-import InfrastructureMaster from "./pages/masters/InfrastructureMaster";
-import AcademicMaster from "./pages/masters/AcademicMaster";
-import CandidateWalkinMaster from "./pages/masters/CandidateWalkinMaster";
 
 // Institute master forms
 import SchoolForm from "./pages/forms/instituteMaster/SchoolForm";
@@ -38,28 +35,8 @@ import RoleForm from "./pages/forms/navigationMaster/RoleForm";
 //Designation Master forms
 import DesignationForm from "./pages/forms/DesignationMaster/DesignationForm";
 
-//Transcript Master forms
-import TranscriptForm from "./pages/forms/TranscriptMaster/TranscriptForm";
-import TranscriptAssignmentForm from "./pages/forms/TranscriptMaster/TranscriptAssignmentForm";
-
-// InventoryMaster Forms
-import StoreForm from "./pages/forms/Inventory Master/StoreForm";
-import MeasureForm from "./pages/forms/Inventory Master/MeasureForm";
-
 // ShiftMaster Forms
 import ShiftForm from "./pages/forms/ShiftMaster/ShiftForm";
-
-// InfrastructureMaster Forms
-import FacilityForm from "./pages/forms/InfrastructureMaster/FacilityForm";
-import BlockForm from "./pages/forms/InfrastructureMaster/BlockForm";
-import RoomForm from "./pages/forms/InfrastructureMaster/RoomForm";
-
-// Academic Master forms
-import SectionForm from "./pages/forms/AcademicMaster/SectionForm";
-import BatchForm from "./pages/forms/AcademicMaster/BatchForm";
-
-// CandidateWalkinMaster Forms
-import ApplicationForm from "./pages/forms/CandidateWalkinMaster/ApplicationForm";
 
 function App() {
   return (
@@ -196,55 +173,6 @@ function App() {
                   path="/DesignationMaster/Designation/Update/:id"
                   element={<DesignationForm />}
                 />
-                {/* Transcript Master */}
-
-                <Route
-                  exact
-                  path="/TranscriptMaster"
-                  element={<TranscriptMaster />}
-                />
-                <Route
-                  exact
-                  path="/TranscriptMaster/Transcript/New"
-                  element={<TranscriptForm />}
-                />
-                <Route
-                  exact
-                  path="/TranscriptMaster/Transcript/Update/:id"
-                  element={<TranscriptForm />}
-                />
-                <Route
-                  exact
-                  path="/TranscriptMaster/TranscriptAssignment/Assign"
-                  element={<TranscriptAssignmentForm />}
-                />
-
-                {/* Inventory Master */}
-                <Route
-                  exact
-                  path="/InventoryMaster"
-                  element={<InventoryMaster />}
-                />
-                <Route
-                  exact
-                  path="/InventoryMaster/Store/New"
-                  element={<StoreForm />}
-                />
-                <Route
-                  exact
-                  path="/InventoryMaster/Store/Update/:id"
-                  element={<StoreForm />}
-                />
-                <Route
-                  exact
-                  path="/InventoryMaster/Measure/New"
-                  element={<MeasureForm />}
-                />
-                <Route
-                  exact
-                  path="/InventoryMaster/Measure/Update/:id"
-                  element={<MeasureForm />}
-                />
 
                 {/* Shift Master */}
 
@@ -259,81 +187,6 @@ function App() {
                   exact
                   path="/ShiftMaster/Shift/Update/:id"
                   element={<ShiftForm />}
-                />
-
-                {/* Infrastructure Master*/}
-                <Route
-                  exact
-                  path="/InfrastructureMaster"
-                  element={<InfrastructureMaster />}
-                />
-                <Route
-                  exact
-                  path="/InfrastructureMaster/Facility/New"
-                  element={<FacilityForm />}
-                />
-                <Route
-                  exact
-                  path="/InfrastructureMaster/Facility/Update/:id"
-                  element={<FacilityForm />}
-                />
-
-                <Route
-                  exact
-                  path="/InfrastructureMaster/Block/New"
-                  element={<BlockForm />}
-                />
-                <Route
-                  exact
-                  path="/InfrastructureMaster/Block/Update/:id"
-                  element={<BlockForm />}
-                />
-                <Route
-                  exact
-                  path="/InfrastructureMaster/Rooms/New"
-                  element={<RoomForm />}
-                />
-                <Route
-                  exact
-                  path="/InfrastructureMaster/Rooms/Update/:id"
-                  element={<RoomForm />}
-                />
-                {/*Academic Master*/}
-                <Route
-                  exact
-                  path="/AcademicMaster"
-                  element={<AcademicMaster />}
-                />
-                <Route
-                  exact
-                  path="/AcademicMaster/Section/New"
-                  element={<SectionForm />}
-                />
-                <Route
-                  exact
-                  path="/AcademicMaster/Section/Update/:id"
-                  element={<SectionForm />}
-                />
-                <Route
-                  exact
-                  path="/AcademicMaster/Batch/New"
-                  element={<BatchForm />}
-                />
-                <Route
-                  exact
-                  path="/AcademicMaster/Batch/Update/:id"
-                  element={<BatchForm />}
-                />
-                {/* CandidateWalkin Master */}
-                <Route
-                  exact
-                  path="/CandidateWalkinMaster"
-                  element={<CandidateWalkinMaster />}
-                />
-                <Route
-                  exact
-                  path="/CandidateWalkinMaster/Candidate/New"
-                  element={<ApplicationForm />}
                 />
               </>
             </Route>
--- src/services/Api.js
+++ src/services/Api.js
@@ -9,15 +9,16 @@ const ApiUrl = `http://192.168.0.179:8080/api`;
 
 // Staging backend
 // const ApiUrl = `https://www.stageapi-acharyainstitutes.in/api`;
-
-axios.interceptors.request.use(
-  (config) => {
-    config.headers.authorization = `Bearer  ${x.token}`;
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
+if (demo1) {
+  axios.interceptors.request.use(
+    (config) => {
+      config.headers.authorization = `Bearer  ${x.token}`;
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
+}
 
 export default ApiUrl;
